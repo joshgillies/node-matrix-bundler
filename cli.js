a234ba33d807227445a3d0ae75906597579ccc0f
Josh Gillies <github@joshgilli.es>
2015-06-25 23:38:09 +1000

    Add command line interface

---
+++ cli.js
@@ -0,0 +1,23 @@
+var argv = require('minimist')(process.argv.slice(2))
+
+if (!argv.length || argv.h || argv.help) {
+  console.log(help())
+}
+
+function help () {
+  return [
+    'Usage:',
+    '  matrix-bundler [entry] [opts]',
+    '',
+    'Options:',
+    '  --help, -h           show help message',
+    '  --entry, -e          the enrty point for bundler, default "."',
+    '  --output, -o         the exported file bundle, default "./export.tgz"',
+    '  --parent, -p         an asset id to import the files under, default "1"',
+    '  --link, -l           the link type all assets will be imported as, default "TYPE_1"',
+    '  --unrestricted, -u   whether unrestricted access is allowed, default "false"',
+    '',
+    'Examples:',
+    '  matrix-bundler --entry ./files --link 2 --unrestricted --output ./files.tgz'
+  ].join('\n')
+}
